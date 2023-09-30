5775b14b3d9826633ea89cb532632b177b5eaf31
lematusiak <leematusiak@gmail.com>
2023-09-30 10:47:46 +0200

    Zadanie nr 4 (30.09.2023)

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Zadanie nr 4 (30.09.2023)
+
+var name = "Ewa";
+var sex = "kobieta";
+var age = 33;
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if(name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");  
+}
+else if (sex != "kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (sex == "kobieta" && age > 10 && age < 20)
+{
+    Console.WriteLine("Nastolatka");
+}
