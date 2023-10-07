514887dfcd515381cb7e437831fdc9454c160b43
lematusiak <leematusiak@gmail.com>
2023-10-07 15:46:38 +0200

    2023.10.07 Zadanie nr 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,23 +1,75 @@
-﻿
-// Zadanie nr 4 (30.09.2023)
+﻿// 2023.10.07 Zadanie nr 5
 
-var name = "Ewa";
-var sex = "kobieta";
-var age = 33;
+int number = 780255628;
+string numberAsString = number.ToString();
+char [] letters = numberAsString.ToArray();
 
-if (sex == "kobieta" && age < 30)
+int count0 = 0;
+int count1 = 0;
+int count2 = 0;
+int count3 = 0;
+int count4 = 0;
+int count5 = 0;
+int count6 = 0;
+int count7 = 0;
+int count8 = 0;
+int count9 = 0;
+
+foreach (char letter in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if(name == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");  
-}
-else if (sex != "kobieta" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else if (sex == "kobieta" && age > 10 && age < 20)
-{
-    Console.WriteLine("Nastolatka");
-}
+    if (letter == '0')
+    {
+        count0++;
+    }
+    else if (letter == '1')
+    {
+        count1++;
+    }
+    else if (letter == '2')
+    {
+        count2++;
+    }
+
+    else if (letter == '3')
+    {
+        count3++;
+    }
+    else if (letter == '4')
+    {
+        count4++;
+    }
+
+    else if (letter == '5')
+    {
+        count5++;
+    }
+    else if (letter == '6')
+    {
+        count6++;
+    }
+
+    else if (letter == '7')
+    {
+        count7++;
+    }
+    else if (letter == '8')
+    {
+        count8++;
+    }
+    else if (letter == '9')
+    {
+        count9++;
+    }
+};
+
+Console.WriteLine("Wynik dla liczby:" + number);
+Console.WriteLine("0 =>" + count0);
+Console.WriteLine("1 =>" + count1);
+Console.WriteLine("2 =>" + count2);
+Console.WriteLine("3 =>" + count3);
+Console.WriteLine("4 =>" + count4);
+Console.WriteLine("5 =>" + count5);
+Console.WriteLine("6 =>" + count6);
+Console.WriteLine("7 =>" + count7);
+Console.WriteLine("8 =>" + count8);
+Console.WriteLine("9 =>" + count9);
